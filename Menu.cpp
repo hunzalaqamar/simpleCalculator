177142dfed4dc952ec647952d29c702d024ffd7b
Hunzala Qamar <hunzla.qamar@gmail.com>
2020-08-03 19:48:28 +0500

    added menu for simpleCalculator

---
+++ Menu.cpp
@@ -0,0 +1,10 @@
+int a,b,c;
+    cout<<"Enter Two Numbers:";
+    cin>>a>>b;
+
+    cout<<"1:Sum Of Numbers";
+    cout<<"\n2:Multiplication Of Numbers";
+    cout<<"\n3:Subtraction Of Numbers";
+    cout<<"\n4:Divison OF Numbers";
+    cout<<"\nEnter Your Choice [1-4] = ";
+    cin>>c;
